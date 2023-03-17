3553caf083fc2133dc9843068c840752eaef332a
BlisS <fixtergeek@gmail.com>
2023-03-17 14:12:14 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://hectorbliss.github.io/vite-deploy'
+  base:'https://youtube.hectorbliss.com/'
 })
